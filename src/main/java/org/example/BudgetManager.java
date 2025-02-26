a13020fdeb09b23a35949cadf6d78bb74e24e01d
Hylke <hylkest2002@gmail.com>
2025-02-26 21:34:16 +0100

    Add budgetmanager class

--- src/main/java/org/example/BudgetManager.java
+++ src/main/java/org/example/BudgetManager.java
@@ -1,4 +1,26 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BudgetManager {
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void printTransactions() {
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+    }
+
+    public double calculateBalance() {
+        double balance = 0;
+        for (Transaction t : transactions) {
+            balance += t.getAmount();
+        }
+        return balance;
+    }
 }
